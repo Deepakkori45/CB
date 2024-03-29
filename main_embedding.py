1086bba432d4d3e91351129656aecf36f4aa30e0
Deepak kori <111627339+Deepakkori45@users.noreply.github.com>
2024-03-29 19:32:11 +0530

    Update main_embedding.py

--- main_embedding.py
+++ main_embedding.py
@@ -1,28 +1,34 @@
 import os
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.chat_models import AzureChatOpenAI
+from langchain_community.chat_models import AzureChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain.chains import ConversationalRetrievalChain
-from dotenv import load_dotenv
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.document_loaders import PyPDFLoader ,TextLoader
+# from dotenv import load_dotenv
+# from dotenv import load_dotenv
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.document_loaders import PyPDFLoader,TextLoader
 from langchain.prompts import PromptTemplate
 from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
 
-load_dotenv()
+# load_dotenv()
 # OPENAI_API_TYPE = os.environ['OPENAI_API_TYPE']
 # OPENAI_API_BASE = os.environ['OPENAI_API_BASE']
 # OPENAI_API_VERSION = os.environ['OPENAI_API_VERSION']
-OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
-DEPLOYEMENT_NAME = os.environ['DEPLOYEMENT_NAME']
-
+# OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+# DEPLOYEMENT_NAME = os.environ['DEPLOYEMENT_NAME']
+# OPENAI_API_TYPE = 'gpt-3'
+# OPENAI_API_BASE = '//api.openai.com'
+# OPENAI_API_VERSION = 'v1'
+GOOGLE_API_KEY = "AIzaSyC5jVGT9OHx4soEsliU60ByZsieobJPRms"
+genai.configure(api_key=GOOGLE_API_KEY)
 class chat_gen():
     def __init__(self):
         self.chat_history=[]
-
 
     def load_doc(self,document_path):
         loader = PyPDFLoader(document_path)
@@ -30,19 +36,20 @@ class chat_gen():
         # Split document in chunks
         text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=30, separator="\n")
         docs = text_splitter.split_documents(documents=documents)
-        embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+        # embeddings = uggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
         # Create vectors
         vectorstore = FAISS.from_documents(docs, embeddings)
         # Persist the vectors locally on disk
         vectorstore.save_local("faiss_index_datamodel")
 
         # Load from local storage
-        persisted_vectorstore = FAISS.load_local("faiss_index_datamodel", embeddings)
+        persisted_vectorstore = FAISS.load_local("faiss_index_datamodel", embeddings,allow_dangerous_deserialization=True)
         return persisted_vectorstore
 
-
+ 
     def load_model(self,):
-        llm = AzureChatOpenAI(deployment_name=DEPLOYEMENT_NAME,
+        llm = AzureChatOpenAI(model = "gemini-pro",
                             temperature=0.0,
                             max_tokens=4000,
                             )
@@ -62,7 +69,7 @@ class chat_gen():
         prompt = PromptTemplate.from_template(template)
         chain = ConversationalRetrievalChain.from_llm(
             llm=llm,
-            retriever=self.load_doc("https://raw.githubusercontent.com/Deepakkori45/CB/main/GenAI.pdf").as_retriever(),
+            retriever=self.load_doc(r"C:\Users\DEEPAK KORI\Desktop\RAG-gpt\data\Offer agreement - Deepak Raju Kori.pdf").as_retriever(),
             #condense_question_prompt=prompt,
             combine_docs_chain_kwargs={'prompt': prompt},
             chain_type="stuff",
@@ -78,5 +85,5 @@ class chat_gen():
 
 if __name__ == "__main__":
     chat = chat_gen()
-    print(chat.ask_pdf("who is charlie chaplin"))
-    print(chat.ask_pdf("when did he die?"))
+    # print(chat.ask_pdf("who is charlie chaplin"))
+    # print(chat.ask_pdf("when did he die?"))
